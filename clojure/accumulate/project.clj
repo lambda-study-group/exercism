19d0caf81337a8ab4531416668e0fda966345504
Mike Cifelli <mcifelli@aquent.com>
2018-10-17 09:47:48 -0400

    Revert "Add 'accumulate' in clojure"
    This reverts commit 9d6d523a2ab559117d9252656d367ff327f24add.

--- clojure/accumulate/project.clj
+++
@@ -1,4 +0,0 @@
-(defproject accumulate "0.1.0-SNAPSHOT"
-  :description "accumulate exercise."
-  :url "https://github.com/exercism/clojure/tree/master/exercises/accumulate"
-  :dependencies [[org.clojure/clojure "1.9.0"]])
