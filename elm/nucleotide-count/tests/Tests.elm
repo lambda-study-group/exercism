3fdf0a874ce1140bbf27875599a75bb681bf5bb3
Panagiotis Georgiadis <panagiwtis.geor@gmail.com>
2019-10-20 18:53:09 +0100

    [Elm] Add "Nucleotide Count" solution

---
+++ elm/nucleotide-count/tests/Tests.elm
@@ -0,0 +1,23 @@
+module Tests exposing (tests)
+
+import Expect
+import NucleotideCount exposing (nucleotideCounts)
+import Test exposing (..)
+
+
+tests : Test
+tests =
+    describe "NucleotideCount"
+        [ test "empty dna strand has no nucleotides" <|
+            \() ->
+                Expect.equal { a = 0, t = 0, c = 0, g = 0 }
+                    (nucleotideCounts "")
+        , test "repetitive sequence has only guanine" <|
+            \() ->
+                Expect.equal { a = 0, t = 0, c = 0, g = 8 }
+                    (nucleotideCounts "GGGGGGGG")
+        , test "counts all nucleotides" <|
+            \() ->
+                Expect.equal { a = 20, t = 21, c = 12, g = 17 }
+                    (nucleotideCounts "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC")
+        ]
