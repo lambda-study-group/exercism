2fc3db842a304944c9c58aee3100984a764f7867
Herminio Torres <herminiocesar@gmail.com>
2020-09-26 14:34:04 -0300

    add rna-transcription

---
+++ elixir/rna-transcription/.elixir_ls/build/test/lib/rna_transcription/ebin/rna_transcription.app
@@ -0,0 +1,6 @@
+{application,rna_transcription,
+             [{applications,[kernel,stdlib,elixir,logger]},
+              {description,"rna_transcription"},
+              {modules,['Elixir.RnaTranscription']},
+              {registered,[]},
+              {vsn,"0.1.0"}]}.
---
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -0,0 +1,25 @@
+defmodule RnaTranscription do
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RnaTranscription.to_rna('ACTG')
+  'UGAC'
+  """
+
+  @nucleotides_list %{
+    ?G => ?C,
+    ?C => ?G,
+    ?T => ?A,
+    ?A => ?U
+  }
+
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    dna
+    |> Enum.map(&gets_rnd/1)
+  end
+
+  defp gets_rnd(dna), do: @nucleotides_list[dna]
+end
